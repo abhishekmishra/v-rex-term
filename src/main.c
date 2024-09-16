4eadd309ba2dba998c5a3b6ef6108360cdc9b1d2
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-09-16 22:51:49 +0530

    initial vcpkg based build

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c - main program of the v-rex-term program
+ *      A terminal client for Docker using the clipdocker library.
+ *
+ * author - Abhishek Mishra
+ * date - 16/09/2024
+ */
+
+int main(int argc, char *argv[])
+{
+    printf("v-rex-term\n");
+    return 0;
+}
